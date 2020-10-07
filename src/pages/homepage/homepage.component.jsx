9a52523b2e2b39f43b8b052efa5c1a35c5a018d6
gelezhinis <gelezhinis@gmail.com>
2020-10-07 09:49:24 +0300

    done the basic routing on the menu-item

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-import MenuItem from '../../components/menu-item/menu-item.component';
+// import MenuItem from '../../components/menu-item/menu-item.component';
 import Directory from '../../components/directory/directory.component';
 
 import './homepage.styles.scss';  
 
 const HomePage = () => (
    <div className='homepage'>
-      <Directory>
-         <MenuItem />
-      </Directory>
+      <Directory />
    </div>
 );
 
