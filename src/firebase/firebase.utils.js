5f8269cd4e28e55f89623a1ed69eeafc1fcf4ffa
gelezhinis <gelezhinis@gmail.com>
2020-10-13 19:01:06 +0300

    added the sign-up component, impplemented firebase utils, included abillity to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,9 +12,36 @@ const config = {
    appId: "1:475696095953:web:db67ff2630eafe11861bf0",
    measurementId: "G-EYP1ELPCQ0"
  };
-
+ 
  firebase.initializeApp(config);
 
+ export const createUserProfileDocument = async (userAuth, additionalData) => {
+   if(!userAuth) return;
+
+   const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+   const snapShot = await userRef.get();
+  //  console.log(userAuth.uid);
+   
+   if(!snapShot.exists) {
+     const { displayName, email } = userAuth;
+     const createdAt = new Date();
+      
+    try {
+      await userRef.set({
+        displayName, 
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch(error) {
+      console.log('error creating user', error.message);
+    }
+  }
+  
+  return userRef; 
+};
+  
  export const auth = firebase.auth();
  export const firestore = firebase.firestore();
 
